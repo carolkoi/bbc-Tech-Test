875031a2adc1c99b3db76599ea6452f4cfbe654d
Caroline Gitonga <caroline.gitonga@autodesk.com>
2022-03-02 14:51:55 +0300

    adding the git repo

--- src/components/Navbars/AuthNavbar.vue
+++ src/components/Navbars/AuthNavbar.vue
@@ -66,7 +66,7 @@
           <li class="flex items-center">
             <a
               class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="https://github.com/creativetimofficial/vue-notus?ref=vn-auth-navbar"
+              href="https://github.com/carolkoi/bbc-Tech-Test/tree/master"
               target="_blank"
             >
               <i
